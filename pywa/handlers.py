728dcd34f86ca0f36dfd2ae01c98220b2a23add4
David Lev <davidlev050414@gmail.com>
2023-06-18 18:09:11 +0300

    [handlers] adding event handlers

---
+++ pywa/handlers.py
@@ -0,0 +1,52 @@
+from typing import Callable, Any, TYPE_CHECKING, Iterable
+from pywa.types import Message, CallbackButtonReply, CallbackListReply
+if TYPE_CHECKING:
+    from pywa import WhatsApp
+
+
+class Handler:
+    __handler_type__ = None
+
+    def __init__(
+            self,
+            handler: Callable[["WhatsApp", Any], Any],
+            filters: Iterable[Callable[["WhatsApp", Any], bool]]
+    ):
+        self.handler = handler
+        self.filters = filters
+
+    def __call__(self, wa: "WhatsApp", data: Any):
+        if all([f(wa, data) for f in self.filters]) if self.filters else True:
+            self.handler(wa, data)
+
+
+class MessageHandler(Handler):
+    __handler_type__ = Message
+    """A message handler (e.g. text, image, video, audio, etc.)."""
+
+
+# class CommandHandler(Handler):
+#     """A command handler (e.g. /start)."""
+#
+#     def __init__(
+#             self,
+#             handler: Callable[["WhatsApp", Any], Any],
+#             commands: str | Iterable[str],
+#             filters: Iterable[Callable[["WhatsApp", Any], bool]]
+#     ):
+#         if isinstance(commands, str):
+#             filters = ((lambda msg: msg.text == commands), *(filters if filters else ()))
+#         elif isinstance(commands, Iterable):
+#             filters = (((lambda msg: msg.text == command.removeprefix("/")) for command in commands),
+#                        *(filters if filters else ()))
+#         super().__init__(handler=handler, filters=filters)
+
+
+class ButtonCallbackHandler(Handler):
+    __handler_type__ = CallbackButtonReply
+    """A button handler."""
+
+
+class SelectionCallbackHandler(Handler):
+    __handler_type__ = CallbackListReply
+    """A section handler."""
