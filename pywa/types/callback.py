0d9d66522fcd78b47e7ce7d733b0397d26764f69
David Lev <davidlev050414@gmail.com>
2023-07-12 11:11:33 +0300

    [callbacks] reply to callback button original message

--- pywa/types/callback.py
+++ pywa/types/callback.py
@@ -28,6 +28,11 @@ class CallbackButton(BaseUpdate):
     data: str
     title: str
 
+    @property
+    def message_id_to_reply(self) -> str:
+        """The ID of the message to reply to."""
+        return self.reply_to_message.message_id
+
     @classmethod
     def from_dict(cls, client: WhatsApp, value: dict):
         message = value['messages'][0]
