3626bf185c397b04e0b34eb56680ffdaa73fc973
David Lev <davidlev050414@gmail.com>
2023-08-13 01:59:05 +0300

    [types,message_status] adding conversation details to `MessageStatus`

--- pywa/types/__init__.py
+++ pywa/types/__init__.py
@@ -4,5 +4,5 @@ This package contains all the types used in the library.
 from .message import Message
 from .others import Contact, MessageType, User, Reaction, Location, Metadata, ReplyToMessage, Order, Product, System
 from .callback import CallbackButton, CallbackSelection, InlineButton, SectionRow, Section, SectionList
-from .message_status import MessageStatus, MessageStatusType
+from .message_status import MessageStatus, MessageStatusType, Conversation, ConversationCategory
 from .media import MediaUrlResponse
--- pywa/types/message_status.py
+++ pywa/types/message_status.py
@@ -17,6 +17,18 @@ class MessageStatusType(_StrEnum):
     DELIVERED = 'delivered'
     READ = 'read'
     FAILED = 'failed'
+
+    def _missing_(self, key):
+        return self.FAILED
+
+
+class ConversationCategory(_StrEnum):
+    """Conversation category."""
+    AUTHENTICATION = 'authentication'
+    MARKETING = 'marketing'
+    UTILITY = 'utility'
+    SERVICE = 'service'
+    REFERRAL_CONVERSION = 'referral_conversion'
     UNKNOWN = 'unknown'
 
     def _missing_(self, key):
@@ -36,9 +48,11 @@ class MessageStatus(BaseUpdate):
         status: The status of the message.
         timestamp: The timestamp when the status was updated.
         from_user: The user who the message was sent to.
+        conversation: The conversation the given status notification belongs to (Optional).
         error: The error that occurred (if status is ``failed``).
     """
     status: MessageStatusType
+    conversation: Conversation | None
     error: WhatsAppError | None
 
     @classmethod
@@ -53,6 +67,33 @@ class MessageStatus(BaseUpdate):
             status=status_type,
             timestamp=datetime.fromtimestamp(int(status['timestamp'])),
             from_user=User(wa_id=status['recipient_id'], name=None),
+            conversation=Conversation.from_dict(status['conversation']) if 'conversation' in status else None,
             error=WhatsAppError.from_incoming_error(status['errors'][0])
             if status_type == MessageStatusType.FAILED else None
+        )
+
+
+@dataclass(frozen=True, slots=True)
+class Conversation:
+    """
+    Represents a conversation.
+
+    - See more: https://developers.facebook.com/docs/whatsapp/pricing#conversations
+
+    Attributes:
+        id: The ID of the conversation.
+        category: The category of the conversation.
+        expiration: The expiration date of the conversation (Optional, only for `sent` updates).
+
+    """
+    id: str
+    category: ConversationCategory
+    expiration: datetime | None
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(
+            id=data['id'],
+            category=ConversationCategory(data['origin']['type']),
+            expiration=datetime.fromtimestamp(int(data['expiration_timestamp'])) if 'expiration_timestamp' in data else None
         )
